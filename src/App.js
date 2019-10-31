dd3808c3e96062e538e7c03923d738f6b63efd0f
Saka Sai Trinath <saitrinath@inviz.ai>
2019-10-31 23:13:22 +0530

    [SakaSaiTrinath] added linting, and intial code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from "react";
-import { Header } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Grid, Menu, Header } from "semantic-ui-react";
 import "./App.css";
 
 function App() {
-	return (
-		<div>
-			<Header>TechGana</Header>
-		</div>
-	);
+  const [activeItem, setActiveItem] = useState("Home");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <Menu pointing>
+              <Menu.Item>
+                <Header as="h2">TechGana</Header>
+              </Menu.Item>
+              <Menu.Menu position="right">
+                <Menu.Item
+                  name="Home"
+                  active={activeItem === "Home"}
+                  onClick={handleItemClick}
+                />
+                <Menu.Item
+                  name="Projects"
+                  active={activeItem === "Projects"}
+                  onClick={handleItemClick}
+                />
+                <Menu.Item
+                  name="Members"
+                  active={activeItem === "Members"}
+                  onClick={handleItemClick}
+                />
+                <Menu.Item
+                  name="Resources"
+                  active={activeItem === "Resources"}
+                  onClick={handleItemClick}
+                />
+                <Menu.Item
+                  name="My Profile"
+                  active={activeItem === "My Profile"}
+                  onClick={handleItemClick}
+                />
+              </Menu.Menu>
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
